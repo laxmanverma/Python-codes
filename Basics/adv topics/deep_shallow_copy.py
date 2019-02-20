344dab7d095279f65a3f380b0405300354005089
Laxman Verma <laxmanbravolook@gmail.com>
2019-02-20 14:32:37 +0530

    diff bw shallow and deep copy

--- Basics/adv topics/deep_shallow_copy.py
+++ Basics/adv topics/deep_shallow_copy.py
@@ -9,7 +9,15 @@ list_2[0] = 'a'
 print(list_1)
 print(list_2)
 print("************************")
+'''
+The difference between shallow and deep copying is only relevant for compound objects 
+(objects that contain other objects, like lists or class instances):
 
+A shallow copy constructs a new compound object and then (to the extent possible) 
+inserts references into it to the objects found in the original.
+A deep copy constructs a new compound object and then, recursively, inserts copies 
+into it of the objects found in the original
+'''
 #shallow copy it only copies the object nested elements are still referenced
 list_3 = [[1, 2, 3], [4, 5, 6], 7]
 list_4 = copy.copy(list_3)
